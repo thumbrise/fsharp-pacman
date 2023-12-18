0cd55011e45bbafd74fd5077d4fdcf5dcf896126
Ruslan Kokoev <ruslan.kokoev.1999@gmail.com>
2023-12-18 19:05:39 +0300

    init

---
+++ Packman/Program.fs
@@ -0,0 +1,18 @@
+﻿// For more information see https://aka.ms/fsharp-console-apps
+open System
+
+let areaSize = 20
+
+let colorBackground = ConsoleColor.White
+let texturePacman = "@"
+let colorPacman = ConsoleColor.Yellow
+let textureEnemy = "$"
+let colorEnemy = ConsoleColor.Red
+let textureCoin = "*"
+let colorCoin = ConsoleColor.Blue
+
+Console.BackgroundColor <- colorBackground
+Console.ForegroundColor <- colorPacman
+
+// Console.WriteLine(texturePacman)
+Console.Write("first\nsecond")
